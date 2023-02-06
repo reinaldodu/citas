02a86fe8f65995f524c8a8a2cec1278e24db5aec
Reinaldo Duque <reinaldodu@gmail.com>
2023-02-06 08:49:55 -0500

    Actualización usuarios.index

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     
-                    {{-- Botón crear un usuario --}}
+                    {{-- Botón crear un nuevo usuario --}}
                     <div class="flex justify-end mb-5">
                         <a href="{{ route('usuarios.create') }}" class="btn btn-outline btn-primary rounded-full">Crear Usuario</a>
                     </div>
